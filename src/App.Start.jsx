0694ce5c371e2fdceeae617e9a0f980c04cc64ce
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-03 22:49:06 +0200

    start app commit

--- src/App.Start.jsx
+++ src/App.Start.jsx
@@ -1,13 +1,14 @@
-export const Start = () => {
+import React from 'react'
+
+// Start component receives an onStart prop from App.jsx
+export default function Start({ onStart }) {
   return (
-    <button
-      style={{
-        background: "blue",
-        color: "white",
-        fontSize: "1.5rem",
-      }}
-    >
-      START GAME
-    </button>
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h1>Rock Paper Scissors</h1>
+      <button onClick={onStart} style={{ fontSize: "1.5rem", padding: "1rem 2rem" }}>
+        Start Game
+      </button>
+    </div>
   );
-};
+}
+

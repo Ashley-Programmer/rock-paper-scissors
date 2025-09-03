c40ba3df30153d27c2078104f7d931e686e0e1cd
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-03 23:33:22 +0200

    App.Start update

--- src/App.Start.jsx
+++ src/App.Start.jsx
@@ -5,7 +5,7 @@ export default function Start({ onStart }) {
   return (
     <div style={{ textAlign: "center", marginTop: "3rem" }}>
       <h1>Rock Paper Scissors</h1>
-      <button onClick={onStart} style={{ fontSize: "1.5rem", padding: "1rem 2rem" }}>
+      <button onClick={onStart} style={{ fontSize: "1.5rem", padding: "1rem 2rem", cursor: "pointer" }}>
         Start Game
       </button>
     </div>
